e597f20d6bc98b8bc52bda11866545f52ce96e5a
Gabriel <gabriel.gutz@live.com>
2022-11-02 19:51:56 -0400

    added authentication middleware functions

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,42 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const { restoreUser, setTokenCookie, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
+
+// GET /api/restore-user
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
+// router.get(
+//     '/restore-user',
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// // GET /api/require-auth
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// });
 
 module.exports = router;
